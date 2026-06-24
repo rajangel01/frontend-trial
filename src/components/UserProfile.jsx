b71ffc641ac3f3e1a76dddc06efd62805a3d04d7
Raj Kumar <rajangel820764@gmail.com>
2026-06-24 20:32:54 +0530

    UserDashboard updated

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const UserProfile = () => {
   const navigate = useNavigate();
+  const [dashboard, setDashboard] = useState({
+    totalTests: 0,
+    rank: 0,
+    avgScore: 0,
+    bestScore: 0,
+  });
+  const userData = JSON.parse(localStorage.getItem("isLoggedIn"));
+  const userId = userData.userId;
 
   const handleLogout = () => {
     localStorage.removeItem("isLoggedIn");
@@ -10,28 +20,38 @@ const UserProfile = () => {
     window.location.reload();
   };
 
+  useEffect(() => {
+    const getDashboard = async () => {
+      const res = await fetch("https://gateprocs.vercel.app/update-user-dashboard", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: userId,
+        }),
+      });
 
-  const userData = JSON.parse(
-  localStorage.getItem("isLoggedIn")
-);
+      const data = await res.json();
+      setDashboard(data);
+    };
+
+    getDashboard();
+  }, [userId]);
+
+  // const userData = JSON.parse(localStorage.getItem("isLoggedIn"));
   const user = {
     name: userData.name,
     email: userData.email,
-    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqf0Wx4wmsKfLYsiLdBx6H4D8bwQBurWhx5g&s",
-    rank: 25,
-    tests: 48,
-    avgScore: 67,
-    bestScore: 89,
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqf0Wx4wmsKfLYsiLdBx6H4D8bwQBurWhx5g&s",
     userId: userData.userId,
   };
   return (
-    
-      <div className="container-lg mt-5">
+    <div className="container-lg mt-5">
       <div className="card shadow-lg border-0">
         <div className="card-body">
-
           <div className="row align-items-center">
-
             {/* Profile Image */}
             <div className="col-md-3 text-center">
               <img
@@ -49,11 +69,10 @@ const UserProfile = () => {
               <p className="text-muted">{user.email}</p>
 
               <div className="row mt-4">
-
                 <div className="col-6 col-md-3 mb-3">
                   <div className="card text-center bg-primary text-white">
                     <div className="card-body">
-                      <h4>{user.rank}</h4>
+                      <h4>{dashboard.rank}</h4>
                       <small>Rank</small>
                     </div>
                   </div>
@@ -62,7 +81,7 @@ const UserProfile = () => {
                 <div className="col-6 col-md-3 mb-3">
                   <div className="card text-center bg-success text-white">
                     <div className="card-body">
-                      <h4>{user.tests}</h4>
+                      <h4>{dashboard.totalTests}</h4>
                       <small>Tests</small>
                     </div>
                   </div>
@@ -71,7 +90,7 @@ const UserProfile = () => {
                 <div className="col-6 col-md-3 mb-3">
                   <div className="card text-center bg-warning text-dark">
                     <div className="card-body">
-                      <h4>{user.avgScore}%</h4>
+                      <h4>{dashboard.avgScore}</h4>
                       <small>Avg Score</small>
                     </div>
                   </div>
@@ -80,27 +99,22 @@ const UserProfile = () => {
                 <div className="col-6 col-md-3 mb-3">
                   <div className="card text-center bg-danger text-white">
                     <div className="card-body">
-                      <h4>{user.bestScore}%</h4>
+                      <h4>{dashboard.bestScore}</h4>
                       <small>Best Score</small>
                     </div>
                   </div>
                 </div>
-
               </div>
 
-              <button className='btn btn-danger' onClick={handleLogout}>
-            Logout
-          </button>
-
+              <button className="btn btn-danger" onClick={handleLogout}>
+                Logout
+              </button>
             </div>
-
           </div>
-
         </div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default UserProfile
+export default UserProfile;
